6e5d230bf941a2a1437fc8ed755367d64b2a25df
Phu Le <phu3262006@gmail.com>
2025-05-29 19:44:30 -0400

    Implement and wrapping decryption logic inside of a function

---
+++ Server/decrypt.py
@@ -0,0 +1,11 @@
+from cryptography.fernet import Fernet 
+from dotenv import load_dotenv
+import os
+
+#Load encryption key
+load_dotenv()
+key = os.getenv("ENCRYPTION_KEY").encode()
+fernet = Fernet(key)
+
+def decrypt_message(encrypted_data):
+    return fernet.decrypt(encrypted_data)
