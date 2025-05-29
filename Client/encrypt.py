4e3b9a2541919b162c9cae606416ad92af23e9cf
Phu Le <phu3262006@gmail.com>
2025-05-29 19:28:22 -0400

    Wrap encryption logic inside of a function

--- Client/encrypt.py
+++ Client/encrypt.py
@@ -3,22 +3,18 @@ from dotenv import load_dotenv
 import os #Access .env
 
 
-# Laod varibles from.env into the Environment
+# Laod varibles from.env into the Environment.
 load_dotenv()
 
-# Get the encryption key from the environment and encode to bytes
+# Get the encryption key from the environment and encode to bytes.
 key = os.getenv("ENCRYPTION_KEY").encode()
 if key is None:
     raise ValueError("Encryption_Key not found in .env")
 
-# Create a Fernet object with the key
+# Create a Fernet object with the key.
 fernet = Fernet(key)
 
-# Message to encrypt (bytes)
-message = b"Hello World!"
+#Function to encrypt message/file in bytes. 
+def encrypt_message(bytes_data):
+    return fernet.encrypt(bytes_data)
 
-#Encrypt message
-encrypted_message = fernet.encrypt(message)
-
-# Print the encrypted message (in bytes)
-print("Encrypted:", encrypted_message)
