4f9e62ae219ae7e4f4d2835fbba6aa75a8a1d07b
Phu Le <phu3262006@gmail.com>
2025-06-01 19:38:08 -0400

    Modified previous steps to handle receiving length of filename & encrypted data in bytes then unpacking into ints

--- Server/receive_file.py
+++ Server/receive_file.py
@@ -1,5 +1,6 @@
 import socket
 from decrypt import decrypt_data
+import struct
 
 #Host & Port
 HOST = "127.0.0.1" #Localhost
@@ -13,23 +14,40 @@ server_socket.bind((HOST,PORT))
 
 #Start listening for incoming connections (Allowing only one at a time)
 server_socket.listen(1)
-print(f"Listening on {HOST}:{PORT}...")
+print(f"[+]Listening on {HOST}:{PORT}...")
 
 #Accept connection from client
 conn, addr = server_socket.accept()
-print(f"Connection established with {addr}")
+print(f"[+]Connection established with {addr}")
 
-#Receive up to 1024 bytes of data from client
-encrypted_data = conn.recv(1024)
-print(f"Received encrypted data: {encrypted_data}")
+#Step 1: Receive length of filename 
+filename_len_bytes = conn.recv(4)
+filename_len = struct.unpack("!I", filename_len_bytes)[0]
 
-#Decrypt data
+#Step 2: Receive filename
+filename_bytes = conn.recv(filename_len)
+filename = filename_bytes.decode()
+
+#Step 3: Receive length of encrypted data
+data_len_bytes = conn.recv(4)
+data_len = struct.unpack("!I", data_len_bytes)[0]
+
+#Step 4: Receive the encrypted data
+#Because encrypted data may be large, read in chunks
+encrypted_data = b""
+while len(encrypted_data) < data_len:
+    packet = conn.recv(min(4096, data_len - len(encrypted_data)))
+    if not packet:
+        break
+    encrypted_data += packet
+
+#Step 5: Decrypt data
 decrypted_data = decrypt_data(encrypted_data)
 
-with open("receive_file.png", "wb") as file:
+#Step 6:Save the decrypted file
+with open(filename, "wb") as file:
     file.write(decrypted_data)
-#print(f"[+] Decrypted data:  {decrypted_data.decode()}")
-
+print("[+]Successfully saved data")
 #Close connection & Socket
 conn.close()
 server_socket.close()
