fb1e5795755b15274cb3c8e733ce4df738743bcc
Phu Le <phu3262006@gmail.com>
2025-06-05 16:43:46 -0400

    docs: shortening documentations for each chunk of code

--- Server/receive_file.py
+++ Server/receive_file.py
@@ -4,7 +4,7 @@ from decrypt import decrypt_data
 import struct
 import argparse
 
-#Host & Port
+#argparse based CLI 
 parser = argparse.ArgumentParser()
 parser.add_argument("-H", "--host", required=True, help="Host to bind to (default aka local: 127.0.0.1)")
 parser.add_argument("-P", "--port", required=True, help="Port to listen on (default 5001)")
@@ -12,13 +12,9 @@ args = parser.parse_args()
 HOST = args.host
 PORT = args.port
 
-#server socket object using default parameters (IPv4 + TCP)
+#Creating server socket
 server_socket = socket.socket()
-
-#Bind socket to specific host and port
 server_socket.bind((HOST,PORT))
-
-#Start listening for incoming connections (Allowing only one at a time)
 server_socket.listen(1)
 print(f"[+]Listening on {HOST}:{PORT}...")
 
@@ -26,22 +22,19 @@ print(f"[+]Listening on {HOST}:{PORT}...")
 conn, addr = server_socket.accept()
 print(f"[+]Connection established with {addr}")
 
-#Step 1: Receive length of filename 
+#Receive length of filename 
 filename_len_bytes = conn.recv(4)
 filename_len = struct.unpack("!I", filename_len_bytes)[0]
 
-#Step 2: Receive filename
+#Receive filename
 filename_bytes = conn.recv(filename_len)
 filename = filename_bytes.decode()
-#---------------------
+
 #Receive length of encrypted data
-#---------------------
 data_len_bytes = conn.recv(4)
 data_len = struct.unpack("!I", data_len_bytes)[0]
 
-#---------------------
 #Receive encrypted data in chunks
-#---------------------
 encrypted_data = b""
 while len(encrypted_data) < data_len:
     packet = conn.recv(min(4096, data_len - len(encrypted_data)))
@@ -49,16 +42,14 @@ while len(encrypted_data) < data_len:
         break
     encrypted_data += packet
 print("[+]Data has been received")
-#---------------------
+
 #Decrypt and save data
-#---------------------
 decrypted_data = decrypt_data(encrypted_data)
 
 with open(filename, "wb") as file:
     file.write(decrypted_data)
 print("[+]Successfully saved data")
-#---------------------
+
 #Close connection & Socket
-#---------------------
 conn.close()
 server_socket.close()
